8725a3503b989d65049d48ad20b65df95fce84a1
quanghuong03 <phamquanghuong03@gmail.com>
2023-09-15 20:32:06 +0700

    quanghuong03

---
+++ polo-man-backend/src/main/java/com/ttn10/poloman/PoloManApplication.java
@@ -0,0 +1,13 @@
+package com.ttn10.poloman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoloManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoloManApplication.class, args);
+    }
+
+}
---
+++ polo-man-backend/src/test/java/com/ttn10/poloman/PoloManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn10.poloman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoloManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
